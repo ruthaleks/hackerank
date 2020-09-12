c2620dc74189f63d227ce8154c162828ba93ed66
ruthaleks <ruth.aleks@gmail.com>
2020-09-12 18:04:12 +0200

    solution to 2D Array -DS

--- hour_glass_sum/main.py
+++ hour_glass_sum/main.py
@@ -1,4 +1,5 @@
 import sys
+import itertools 
 
 def read_input():
     raw_data = sys.stdin.readlines()
@@ -6,10 +7,29 @@ def read_input():
     arr = [tuple(map(int,line.split(" "))) for line in data]
     return arr
 
+# y/x . .
+# .
+# .
+# x=[0, 3]
+# y=[0, 3]
+def hour_glass_sum(arr, c):
+    y = c[0]
+    x = c[1]
+    #print(arr[y][x:x+3])
+    #print(arr[y+1][x+1])
+    #print(arr[y+2][x:x+3])
+    return sum(arr[y][x:x+3]) + arr[y+1][x+1] + sum(arr[y+2][x:x+3]) 
+
+def max_hour_glass_sum(arr): 
+    idx = [i for i in range(4)]
+    c = list(itertools.product(idx,idx))
+    return max([hour_glass_sum(arr, i) for i in c])
+
+
 
 def main():
     arr = read_input()
-    print(arr)
+    print(max_hour_glass_sum(arr))
 
 if __name__ == "__main__":
     main()
