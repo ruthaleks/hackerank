ad06966b82fb777b763e0caba078de70766a771d
ruthaleks <ruth.aleks@gmail.com>
2020-09-12 19:05:39 +0200

    alternative solution using itertools

--- left_rotation/main.py
+++ left_rotation/main.py
@@ -1,3 +1,4 @@
+import itertools as it
 import sys
 
 def read_input():
@@ -9,9 +10,12 @@ def left_rot(n, d, a):
     d = {(n - d + i)%n : a[i] for i in range(n)}
     return [d[i] for i in range(n)]
 
+def left_rot_2(n, d, a):
+    return list(it.islice(it.cycle(a), d, d+n))
+
 def main():
     n, d, a = read_input()
-    res = left_rot(n, d, a)
+    res = left_rot_2(n, d, a)
     print(res)
 
 if __name__ == "__main__":
